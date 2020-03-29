f55dc6e74880997f908befaacfb578ef081d6e80
mdmahbubur007 <61216155+mdmahbubur007@users.noreply.github.com>
2020-03-30 01:12:15 +0600

    firebase.config

---
+++ src/Components/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAeZR_-Mf57GDty-HrR7voT-sTve3oqhyQ",
+    authDomain: "hot-onion-resturent-mahbub.firebaseapp.com",
+    databaseURL: "https://hot-onion-resturent-mahbub.firebaseio.com",
+    projectId: "hot-onion-resturent-mahbub",
+    storageBucket: "hot-onion-resturent-mahbub.appspot.com",
+    messagingSenderId: "978888888575",
+    appId: "1:978888888575:web:b329510bf1ae110e207cca"
+  };
+export default firebaseConfig;
